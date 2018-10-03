ee79614b89609a7d9471b2874a047df19a54c27b
Rok Carl <rokcarl@gmail.com>
2018-10-03 14:03:18 +0200

    Add logo to index

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,11 @@
   <section class="container">
     <div>
       <logo/>
-      <h1 class="title">
-        promet-br-ct
-      </h1>
       <h2 class="subtitle">
         Promet Brezice Catez
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="/promet" class="button--green">Promet</a>
       </div>
     </div>
   </section>
